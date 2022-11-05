b6a7c689bbc9dc31ac7ddcfa4297e08c156981db
Sevy Wagner <sevywagner@Sevys-MacBook-Pro.local>
2022-11-04 19:35:05 -0500

    Footer added

--- src/Components/Layout/Footer.js
+++ src/Components/Layout/Footer.js
@@ -1,7 +1,24 @@
+import styles from './Css/footer.module.css';
+
 const Footer = () => {
     return (
-        <div>
-            <h1>Footer</h1>
+        <div className={styles.footer}>
+            <div className={styles.pics}>
+                <a href='instagram.com'>
+                    <div className={styles.insta}></div>
+                </a>
+                <div>
+                    <div className={styles.shield}></div>
+                </div>
+                <a href='facebook.html'>
+                    <div className={styles.facebook}></div>
+                </a>
+            </div>
+            <div className={styles.copyright}>
+                <p>
+                    &copy;1922-2022 Texas Sammy &#8212; 2501 Leon St, Austin TX, 78705 &#8212; Privacy Policy
+                </p>
+            </div>
         </div>
     );
 }
