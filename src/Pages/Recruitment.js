0d341dfacb27e27602a477c4eb8e45b3d77ed7e0
Sevy Wagner <sevywagner@Sevys-MBP.attlocal.net>
2022-11-08 20:44:04 -0600

    responsive fixes

--- src/Pages/Recruitment.js
+++ src/Pages/Recruitment.js
@@ -12,9 +12,9 @@ const Recruitment = () => {
             <iframe
                 src="https://www.youtube.com/embed/IzsNBPnhcLo" 
                 title="YouTube video player"
-                frameborder="0" 
+                frameBorder='0'
                 allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture" 
-                allowfullscreen 
+                allowFullScreen 
             />
 
             <p className={styles.pg}>
